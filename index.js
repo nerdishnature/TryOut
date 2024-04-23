1097ead048ffe2a56d8dac0d1a616abc642bc0a4
mabuny <111080359+mabuny@users.noreply.github.com>
2024-04-23 19:03:56 +0200

    fix module reference

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import {Todo} from "todo.js"
+import {Todo} from "./todo.js"
 
 function initializeData(){
- let todo = new Todo( );
+ let todo = new Todo(1, "Hans", "Info Hausaufgaben", 2);
+ alert(todo.who + ' - ' + todo.what);
 }
